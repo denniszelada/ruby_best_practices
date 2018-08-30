1a08dc3a713895d4ea18ac8c4b4732428e2e7d84
Dennis Zelada <denniszelada@gmail.com>
2018-08-30 13:15:56 +0200

    Create data_clump.rb

---
+++ data_clump.rb
@@ -0,0 +1,8 @@
+#Data Clump
+
+> A data clump happens when you have a pair of arguments, at least two, that you are passing together all the time;
+> a test to identify a data clump is if you remove one argument the another one make kind of sense? 
+> So the arguments have an implicit relation.
+
+> A way to solve this smell is extract an object with the arguments.
+
